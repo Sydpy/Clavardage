911929e8464f8ab94f3ec0dbc208f0fe3b273ef3
Sydpy <sylvain.dupouy@protonmail.com>
2018-12-13 11:17:20 +0100

    reimplemented BroadcastMessage

--- src/main/java/org/etudinsa/clavardage/users/BroadcastMessage.java
+++ src/main/java/org/etudinsa/clavardage/users/BroadcastMessage.java
@@ -5,17 +5,47 @@ import java.net.InetAddress;
 
 class BroadcastMessage implements Serializable {
 
-    enum Type { USERDB_REQUEST, NEWUSER, USERLEAVING }
+    static enum Type { USERDB_REQUEST, NEWUSER, USERLEAVING }
 
     final Type type;
-    User user;
+    String pseudo;
 
     BroadcastMessage(Type type) {
         this.type = type;
     }
 
-    BroadcastMessage(Type type, User user) {
+    BroadcastMessage(Type type, String pseudo) {
         this.type = type;
-        this.user = user;
+        this.pseudo = pseudo;
+    }
+    
+    public static BroadcastMessage fromString(String str) throws Exception {
+    	
+    	String[] splitted = str.split(":", 2);
+    	
+    	Type type = Type.valueOf(splitted[0].trim());
+    	
+    	if (type == Type.NEWUSER) {
+    		if (splitted.length != 2)
+    			throw new Exception("Invalid string");
+    		
+    		String pseudo = splitted[1].trim();
+    		
+    		return new BroadcastMessage(type, pseudo);
+    	}
+    	
+    	return new BroadcastMessage(type);
+    }
+    
+    @Override
+    public String toString() {
+		StringBuilder sb = new StringBuilder(type.toString());
+		
+		if (pseudo != null) {
+			sb.append(":");
+			sb.append(pseudo);
+		}
+		
+		return sb.toString();
     }
 }
